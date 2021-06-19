254324972bb6fb0ab4a43ef880bc5083dac32b29
Burhan Jawed <BurhanJawed@gmail.com>
2021-06-19 01:44:43 -0400

    fixed projects links in projects page

--- Server/Controllers/projects-sub-pages.js
+++ Server/Controllers/projects-sub-pages.js
@@ -1,16 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.DisplayReaderLab = exports.DisplayRascalFranky = exports.DisplayHolyCow = void 0;
+const Util_1 = require("../Util");
 function DisplayHolyCow(req, res, next) {
-    res.render('projects-sub-pages', { title: 'Holy Cow - Projects - Burhan Jawed', page: 'holy-cow' });
+    res.render('projects-sub-pages', { title: 'Holy Cow - Projects - Burhan Jawed', page: 'holy-cow', displayName: Util_1.UserDisplayName(req) });
 }
 exports.DisplayHolyCow = DisplayHolyCow;
 function DisplayRascalFranky(req, res, next) {
-    res.render('projects-sub-pages', { title: 'Rascal Franky - Projects - Burhan Jawed', page: 'rascal-franky' });
+    res.render('projects-sub-pages', { title: 'Rascal Franky - Projects - Burhan Jawed', page: 'rascal-franky', displayName: Util_1.UserDisplayName(req) });
 }
 exports.DisplayRascalFranky = DisplayRascalFranky;
 function DisplayReaderLab(req, res, next) {
-    res.render('projects-sub-pages', { title: 'ReaderLab - Projects - Burhan Jawed', page: 'readerlab' });
+    res.render('projects-sub-pages', { title: 'ReaderLab - Projects - Burhan Jawed', page: 'readerlab', displayName: Util_1.UserDisplayName(req) });
 }
 exports.DisplayReaderLab = DisplayReaderLab;
 //# sourceMappingURL=projects-sub-pages.js.map
--- Server/Controllers/projects-sub-pages.ts
+++ Server/Controllers/projects-sub-pages.ts
@@ -1,19 +1,22 @@
 import express, { Request, Response, NextFunction } from 'express';
 
+// import Util functions
+import { UserDisplayName } from '../Util';
+
 // Display Holy Cow Page
 export function DisplayHolyCow(req: Request, res: Response, next: NextFunction): void
 {
-    res.render('projects-sub-pages', { title: 'Holy Cow - Projects - Burhan Jawed', page: 'holy-cow' });
+    res.render('projects-sub-pages', { title: 'Holy Cow - Projects - Burhan Jawed', page: 'holy-cow',  displayName: UserDisplayName(req)});
 }
 
 // Display Rascal Franky Page
 export function DisplayRascalFranky(req: Request, res: Response, next: NextFunction): void
 {
-    res.render('projects-sub-pages', { title: 'Rascal Franky - Projects - Burhan Jawed', page: 'rascal-franky' });
+    res.render('projects-sub-pages', { title: 'Rascal Franky - Projects - Burhan Jawed', page: 'rascal-franky', displayName: UserDisplayName(req) });
 }
 
 // Display ReaderLab Page
 export function DisplayReaderLab(req: Request, res: Response, next: NextFunction): void
 {
-    res.render('projects-sub-pages', { title: 'ReaderLab - Projects - Burhan Jawed', page: 'readerlab' });
+    res.render('projects-sub-pages', { title: 'ReaderLab - Projects - Burhan Jawed', page: 'readerlab', displayName: UserDisplayName(req)});
 }
