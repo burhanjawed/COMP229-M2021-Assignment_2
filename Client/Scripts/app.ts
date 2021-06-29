6a92236b4f63be552f7ec47f1f43e15f0eb939a0
Burhan Jawed <BurhanJawed@gmail.com>
2021-06-29 07:16:36 -0400

    added animations to home projects contacts list login and register pages

--- Client/Scripts/app.ts
+++ Client/Scripts/app.ts
@@ -63,6 +63,23 @@
     });
     }
 
+    // fade in scroll
+    $(document).on("scroll", function() {
+        var pageTop = $(document).scrollTop();
+        var pageBottom = pageTop + $(window).height();
+        var tags = $(".tag");
+      
+        for (var i = 0; i < tags.length; i++) {
+          var tag = tags[i];
+      
+          if ($(tag).position().top < pageBottom) {
+            $(tag).addClass("visible");
+          } else {
+            $(tag).removeClass("visible");
+          }
+        }
+      });
+
     window.addEventListener("load", Start);
 })();
 
